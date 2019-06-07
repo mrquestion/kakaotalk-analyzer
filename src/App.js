f7c6bac8abea4270fa53cae749a0505fc6c7e534
Mr. Question Mark <loremipsumquaestio@gmail.com>
2019-06-07 21:27:25 +0900

    change to React.Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="container">
-      <div className="row mt-4">
-        <div className="col col-12 offset-md-3 col-md-6">
-          <div className="custom-file">
-            <label htmlFor="select-file" className="custom-file-label">Select file...</label>
-            <input id="select-file" className="custom-file-input" type="file" />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="row mt-4">
+          <div className="col col-12 offset-md-3 col-md-6">
+            <div className="custom-file">
+              <label htmlFor="select-file" className="custom-file-label">Select file...</label>
+              <input id="select-file" className="custom-file-input" type="file" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
